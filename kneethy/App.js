23100340c0617ea15539318b1eb40f4544b6ecc2
vbatistella <victorabatistella@gmail.com>
2021-09-28 11:29:00 -0300

    draw

--- kneethy/App.js
+++ kneethy/App.js
@@ -1,63 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Icon from 'react-native-vector-icons/Feather';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.background}>
-      <View style={styles.topMenu}>
-        <div>
-          {newIcon('menu', '#fdffff')}
-        </div>
-        <div>
-          <Text style={styles.title}>K N E E T H Y</Text>
-        </div>
-          {newIcon('user', '#fdffff')}
-        <div>
-        </div>
-      </View>
-      <Text style={styles.mainText}>Open up App.js to start working on your apps!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
 
-function newIcon(name, color) {
-  return <Icon name={name} size={30} color={color} />;
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Home"
+        onPress={() => navigation.navigate('Home')}
+      />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    backgroundColor: '#222531',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: "100%"
-  },
-  mainText: {
-    color: '#fdffff',
-    fontFamily: 'arial',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  secText: {
-    color: '#5B5E72',
-    fontFamily: 'arial',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  topMenu: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    flexDirection: 'row',
-    width: '100%',
-  },
-  title: {
-    color: '#fdffff',
-    fontFamily: 'arial',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Perfil = () => {
+  return(<Icon name="person-outline" size={24} color="#fff" style={{ marginLeft: 5, marginRight: 15 }} />)
+}
+
+const Menu = () => {
+  return(<Icon name="menu-outline" size={32} color="#fff" style={{ marginLeft: 5, marginRight: 20 }} />)
+}
+
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home"
+            screenOptions={{
+              headerStyle: {
+                backgroundColor: '#222531',
+              },
+              headerTintColor: '#fff',
+              headerRight: () => <Perfil/>
+            }}
+      >
+        <Drawer.Screen name="Home" component={HomeScreen} options={{headerTitle: "K N E E T H Y"}}/>
+        <Drawer.Screen name="Details" component={DetailsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
